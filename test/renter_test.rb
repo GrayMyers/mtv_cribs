8df880ae6ea0579dfca4097cdecc8141719c4079
Grayson Myers <graymyers10@gmail.com>
2020-10-20 09:07:16 -0600

    Add existence and attribute tests to renter test class

---
+++ test/renter_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require 'minitest/pride'
+
+class RenterTest < Minitest::Test
+  def setup
+    @renter = Renter.new("Jessie")
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Renter, @renter
+
+    assert_equal "Jessie", @renter.name
+  end
+end
