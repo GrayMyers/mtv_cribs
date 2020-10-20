f475e5bf51912c37cc70fe393bdeb0ec4d57ffed
Grayson Myers <graymyers10@gmail.com>
2020-10-20 09:34:53 -0600

    Add renters method to building class

--- lib/building.rb
+++ lib/building.rb
@@ -7,4 +7,11 @@ class Building
   def add_unit(unit)
     @units << unit
   end
+
+  def renters
+    renter_array = @units.map do |unit|
+      unit.renter
+    end
+    renter_array.filter{|renter| renter} #removes nil elements
+  end
 end
