ab7f50d2442e3927b8fcbaaeaa54c9839ca0420d
Grayson Myers <graymyers10@gmail.com>
2020-10-20 10:06:00 -0600

    Add renter_with_highest_rent method to building class

--- lib/building.rb
+++ lib/building.rb
@@ -21,9 +21,22 @@ class Building
       unit.monthly_rent
     end
     if @units.length > 0
-      total_rent/units.length
+      total_rent/@units.length
     else
       0 #dividing by zero is bad
+    end
+  end
+
+  def rented_units
+    @units.filter{|unit| unit.renter} #removes units with no renter
+  end
+
+  def renter_with_highest_rent
+    highest_rented = rented_units.max_by{|unit| unit.monthly_rent}
+    if highest_rented
+      highest_rented.renter
+    else
+      nil
     end
   end
 end
