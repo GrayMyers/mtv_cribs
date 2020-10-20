f3dd3ce1fb79ec828738bfde172536f381cc5f9a
Grayson Myers <graymyers10@gmail.com>
2020-10-20 09:14:43 -0600

    Add apartment class and attributes

---
+++ lib/apartment.rb
@@ -0,0 +1,9 @@
+class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+  def initialize(info_hash)
+    @number = info_hash[:number]
+    @monthly_rent = info_hash[:monthly_rent]
+    @bathrooms = info_hash[:bathrooms]
+    @bedrooms = info_hash[:bedrooms]
+  end
+end
